73f82dd262934daab6f214a4a8b53c7e063f86ad
Siddharth Vadgama <siddv@hotmail.com>
2016-01-26 00:33:40 +0000

    resets controller.

--- server/api/oauth/oauth.controller.js
+++ server/api/oauth/oauth.controller.js
@@ -4,11 +4,8 @@ var _ = require('lodash');
 var https = require('https');
 var http = require('http');
 var querystring = require('querystring');
-//var credentials = require('./credentials.js');
-var credentials = {
-    client: 'oauthclient_000094S7qhLsD1dGgZGVhh',
-    secret: 'V+laHjZX8SgQ5D9eZB879tff3Rvr2rJrF0KMAAsNOl0PoK83qXHS2UOuZxB9YZvnTLGUxBREuIN0bw5HO4Gt'
-};
+var Cookies = require( "cookies" );
+var credentials = require('./credentials.js');
 
 
 exports.index = function (req, res) {
